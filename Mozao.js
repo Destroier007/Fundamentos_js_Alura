accd7156385b5973f41eaffc8c54898d4e59e607
bowie <marketing@letscode.com.br>
2023-06-03 19:43:15 -0300

    será apagado

---
+++ Mozao.js
@@ -0,0 +1,21 @@
+
+const primeiroValor = "21";
+const segundoValor = 21;
+//console.log(primeiroValor === segundoValor)
+
+//console.log("Eu amo meu amor")
+
+// ola, bom dia XXX, seja bem vindo a nossa plataforma.
+
+const Nome = "Nickolas"
+const boasVindas = `ola, bom dia ${Nome}, seja bem vindo a nossa plataforma.`
+//console.log(boasVindas)
+
+const namorado = "Nickolas";
+const namorada = "Amanda";
+const amaMais = "Ama mais";
+const naoAmaMais = "Nao ama";
+const oQuantoAma = 45;
+
+const situaçao = ` ola ${namorado}, gostaria de saber se sua namorada ${namorada} ama mais vc, Resposta: ${oQuantoAma >= 50 ? amaMais:naoAmaMais}`
+console.log(situaçao)
